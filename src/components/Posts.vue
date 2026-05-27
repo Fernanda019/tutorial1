6d6c6a722702530cfdd65c1e4a999ee54760b8f2
Fernanda019 <fernanda05@gmail.com>
2026-05-27 14:22:02 -0500

    Implementacion de lectura de posts

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,3 +1,23 @@
+<script setup>
+import axios from 'axios';
+import { onMounted, ref } from 'vue';
+
+const posts = ref([]);
+
+const getPosts = async () => {
+    try {
+        const response = await axios.get('http://localhost:8080/posts');
+        posts.value = response.data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+onMounted(() => {
+    getPosts();
+});
+</script>
+
 <template>
     <h1>All Post</h1>
 
@@ -8,9 +28,10 @@
             <th>Action</th>
         </tr>
 
-        <tr>
-            <td>1</td>
-            <td>Hello World!</td>
+        <tr v-for="(post, index) in posts" :key="post.id">
+            <td>{{ index + 1 }}</td>
+
+            <td>{{ post.title }}</td>
 
             <td>
                 <button class="btn btn-green">O</button>
